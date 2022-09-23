eaf54ebf75dab01bb218212335ed05479b18eb4d
balaji <86532365+HVBSL@users.noreply.github.com>
2022-09-23 21:26:40 +0530

    Add files via upload

---
+++ Assignment/Junaidh/scripts/main.js
@@ -0,0 +1,17 @@
+<<<<<<< HEAD
+// Animations
+AOS.init({
+  anchorPlacement: 'top-left',
+  duration: 1000
+});
+
+// Add your javascript here
+=======
+// Animations
+AOS.init({
+  anchorPlacement: 'top-left',
+  duration: 1000
+});
+
+// Add your javascript here
+>>>>>>> 8374590 (commit)
